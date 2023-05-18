96a2ecd80432fe83e425bc3691c5dfeb36024a52
Eduard789 <91131632+Eduard789@users.noreply.github.com>
2023-05-18 12:09:46 +0200

    Add files via upload

--- c#calculator/Program.cs
+++ c#calculator/Program.cs
@@ -1,146 +1,206 @@
 ﻿using System;
 using System.Text;
 
-class MatrixCalculator
+namespace MatrixCalculator
 {
-    static void Main()
+    class Program
     {
-        Console.OutputEncoding = Encoding.UTF8; 
-
-        Console.Write("Розмір матриці: ");
-        int size = int.Parse(Console.ReadLine());
-
-        double[,] matrixA = new double[size, size];
-        double[,] matrixB = new double[size, size];
-        double[,] resultMatrix = new double[size, size];
-
-        Console.WriteLine("Елементи матриці A:");
-        ReadMatrix(matrixA);
-
-        Console.WriteLine("Елементи матриці B:");
-        ReadMatrix(matrixB);
-
-        Console.WriteLine("Виберіть операцію:");
-        Console.WriteLine("1 - додавання матриць");
-        Console.WriteLine("2 - віднімання матриць");
-        Console.WriteLine("3 - множення матриць");
-        int operation = int.Parse(Console.ReadLine());
-
-        switch (operation)
+        static void Main()
         {
-            case 1:
-                resultMatrix = AddMatrices(matrixA, matrixB);
-                break;
-            case 2:
-                resultMatrix = SubtractMatrices(matrixA, matrixB);
-                break;
-            case 3:
-                resultMatrix = MultiplyMatrices(matrixA, matrixB);
-                break;
-            default:
-                Console.WriteLine("Некоректний вибір операції.");
-                break;
-        }
+            Console.OutputEncoding = Encoding.UTF8;
 
-        Console.WriteLine("Результат:");
-        PrintMatrix(resultMatrix);
-    }
-
-    static void ReadMatrix(double[,] matrix)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
+            while (true)
             {
-                Console.Write($"Елемент [{i}, {j}]: ");
-                matrix[i, j] = double.Parse(Console.ReadLine());
-            }
-        }
-    }
+                Console.WriteLine("Для генерації випадкової матриці введіть 'random'");
+                Console.WriteLine("Для виходу з програми введіть 'exit'\n");
 
-    static void PrintMatrix(double[,] matrix)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
+                Console.Write("Розмір матриці: ");
+                string input = Console.ReadLine();
 
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                Console.Write(matrix[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
-    }
+                if (input.ToLower() == "exit")
+                    break;
 
-    static double[,] AddMatrices(double[,] matrixA, double[,] matrixB)
-    {
-        int rows = matrixA.GetLength(0);
-        int cols = matrixA.GetLength(1);
+                int size;
+                int[,] matrixA;
+                int[,] matrixB;
 
-        double[,] resultMatrix = new double[rows, cols];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                resultMatrix[i, j] = matrixA[i, j] + matrixB[i, j];
-            }
-        }
-
-        return resultMatrix;
-    }
-
-    static double[,] SubtractMatrices(double[,] matrixA, double[,] matrixB)
-    {
-        int rows = matrixA.GetLength(0);
-        int cols = matrixA.GetLength(1);
-
-        double[,] resultMatrix = new double[rows, cols];
-
-                for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                resultMatrix[i, j] = matrixA[i, j] - matrixB[i, j];
-            }
-        }
-
-        return resultMatrix;
-    }
-
-    static double[,] MultiplyMatrices(double[,] matrixA, double[,] matrixB)
-    {
-        int rowsA = matrixA.GetLength(0);
-        int colsA = matrixA.GetLength(1);
-        int rowsB = matrixB.GetLength(0);
-        int colsB = matrixB.GetLength(1);
-
-        double[,] resultMatrix = new double[rowsA, colsB];
-
-        if (colsA != rowsB)
-        {
-            Console.WriteLine("Множення матриць неможливе.");
-            return resultMatrix;
-        }
-
-        for (int i = 0; i < rowsA; i++)
-        {
-            for (int j = 0; j < colsB; j++)
-            {
-                double sum = 0;
-                for (int k = 0; k < colsA; k++)
+                if (input.ToLower() == "random")
                 {
-                    sum += matrixA[i, k] * matrixB[k, j];
+                    Console.Write("Введіть розмір матриці: ");
+                    string sizeInput = Console.ReadLine();
+
+                    if (!int.TryParse(sizeInput, out size))
+                    {
+                        Console.WriteLine("Некоректний ввід розміру матриці. Спробуйте ще раз.\n");
+                        continue;
+                    }
+
+                    matrixA = GenerateRandomMatrix(size);
+                    matrixB = GenerateRandomMatrix(size);
+
+                    Console.WriteLine("Елементи матриці A:");
+                    PrintMatrix(matrixA);
+
+                    Console.WriteLine("Елементи матриці B:");
+                    PrintMatrix(matrixB);
                 }
-                resultMatrix[i, j] = sum;
+                else
+                {
+                    if (!int.TryParse(input, out size))
+                    {
+                        Console.WriteLine("Некоректний ввід. Спробуйте ще раз.\n");
+                        continue;
+                    }
+
+                    Console.WriteLine("Елементи матриці A:");
+                    matrixA = ReadMatrix(size);
+                    PrintMatrix(matrixA);
+
+                    Console.WriteLine("Елементи матриці B:");
+                    matrixB = ReadMatrix(size);
+                    PrintMatrix(matrixB);
+                }
+
+                Console.WriteLine("Виберіть операцію:");
+                Console.WriteLine("1 - додавання матриць");
+                Console.WriteLine("2 - віднімання матриць");
+                Console.WriteLine("3 - множення матриць");
+                int operation;
+                if (!int.TryParse(Console.ReadLine(), out operation))
+                {
+                    Console.WriteLine("Некоректний ввід операції. Спробуйте ще раз.\n");
+                    continue;
+                }
+
+                int[,] resultMatrix;
+
+                switch (operation)
+                {
+                    case 1:
+                        resultMatrix = AddMatrices(matrixA, matrixB);
+                        break;
+                    case 2:
+                        resultMatrix = SubtractMatrices(matrixA, matrixB);
+                        break;
+                    case 3:
+                        resultMatrix = MultiplyMatrices(matrixA, matrixB);
+                        break;
+                    default:
+                        Console.WriteLine("Некоректний ввід операції. Спробуйте ще раз.\n");
+                        continue;
+                }
+
+                Console.WriteLine("Результат:");
+                PrintMatrix(resultMatrix);
+                Console.WriteLine();
             }
         }
 
-        return resultMatrix;
+        static int[,] GenerateRandomMatrix(int size)
+        {
+            int[,] matrix = new int[size, size];
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = random.Next(0, 100);
+                }
+            }
+
+            return matrix;
+        }
+        
+        static int[,] ReadMatrix(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($"Елемент [{i + 1},{j + 1}]: ");
+                    string input = Console.ReadLine();
+                    int value;
+
+                    if (!int.TryParse(input, out value))
+                    {
+                        Console.WriteLine("Некоректний ввід елементу матриці. Спробуйте ще раз.\n");
+                        j--;
+                        continue;
+                    }
+
+                    matrix[i, j] = value;
+                }
+            }
+
+            return matrix;
+        }
+
+        static int[,] AddMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int size = matrixA.GetLength(0);
+            int[,] result = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = matrixA[i, j] + matrixB[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        static int[,] SubtractMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int size = matrixA.GetLength(0);
+            int[,] result = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = matrixA[i, j] - matrixB[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        static int[,] MultiplyMatrices(int[,] matrixA, int[,] matrixB)
+        {
+            int size = matrixA.GetLength(0);
+            int[,] result = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    for (int k = 0; k < size; k++)
+                    {
+                        result[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
-
